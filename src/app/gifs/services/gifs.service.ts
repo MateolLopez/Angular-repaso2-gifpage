1c0bcbe666fa03b291d26bfca46e8087872ae58f
MateolLopez <80400141+MateolLopez@users.noreply.github.com>
2022-02-05 10:51:15 -0300

    end

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey: string = 'AtchsOjOn0ZSKH9bhCrcX6hmIGjMAoSI';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+  private _record: string[] = [];
+
+  public results: Gif[] = [];
+
+  get record(){
+    return [...this._record];
+  }
+
+  constructor( private http: HttpClient ) {
+
+    if(localStorage.getItem('record')){
+      this._record = JSON.parse(localStorage.getItem('record')!);
+      this.results = JSON.parse(localStorage.getItem('results')!);
+
+    }
+
+  }
+
+  searchGifs(query: string = ''){
+
+    query = query.trim().toLowerCase();
+
+    if(!this._record.includes(query)){
+      this._record.unshift(query);
+      this._record = this._record.splice(0,10)
+
+      localStorage.setItem('record', JSON.stringify(this._record));
+    }
+
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('limit', '10')
+          .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${this.serviceUrl}/search`,{params})
+      .subscribe(resp =>{
+        console.log(resp.data);
+        this.results = resp.data;
+        localStorage.setItem('results', JSON.stringify(this.results));
+    })
+
+  }
+
+}
